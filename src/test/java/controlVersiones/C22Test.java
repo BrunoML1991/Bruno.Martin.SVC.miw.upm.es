28b3379d89953db6f9d9dbba402ca0e89411053a
Bruno Martin <=>
2018-09-21 17:35:06 +0200

    Created C22Test with testMA()

---
+++ src/test/java/controlVersiones/C22Test.java
@@ -0,0 +1,21 @@
+package controlVersiones;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class C22Test {
+
+    private C22 c22;
+
+    @BeforeEach
+    void beforeEach (){
+        c22 = new C22();
+    }
+
+    @Test
+    void testMA (){
+        assertEquals("mA",c22.mA());
+    }
+
+}
