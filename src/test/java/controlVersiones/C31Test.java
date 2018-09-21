d0a3bc93e08696e6ce4875b2831e79d06da56cac
Bruno Martin <=>
2018-09-21 18:11:45 +0200

    Created C31Test with testM1() and testM2 ()

---
+++ src/test/java/controlVersiones/C31Test.java
@@ -0,0 +1,26 @@
+package controlVersiones;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class C31Test {
+
+    private C31 c31;
+
+    @BeforeEach
+    void beforeach (){
+        c31 = new C31();
+    }
+
+    @Test
+    void testM1 (){
+        assertEquals("m1",c31.m1());
+    }
+
+    @Test
+    void testM2 (){
+        assertEquals("m2",c31.m2());
+    }
+
+}
