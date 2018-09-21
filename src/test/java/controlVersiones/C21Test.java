9eab4a33225a6f3f2ce26ef1acbf5c5505292e8f
Bruno Martin <=>
2018-09-21 17:19:57 +0200

    Created C21Test with testM1() and testM2 ()

---
+++ src/test/java/controlVersiones/C21Test.java
@@ -0,0 +1,26 @@
+package controlVersiones;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class C21Test {
+
+    private C21 c21;
+
+    @BeforeEach
+    void beforeach (){
+        c21 = new C21();
+    }
+
+    @Test
+    void testM1 (){
+        assertEquals("m1",c21.m1());
+    }
+
+    @Test
+    void testM2 (){
+        assertEquals("m2",c21.m2());
+    }
+
+}
